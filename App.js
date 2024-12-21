efba2d7eb3c7f85551dc9e685c693aeac40d9201
Mehboob Alam <calltoexperts@gmail.com>
2024-12-21 08:13:30 +0500

    Learning mobile applications development details

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React from "react";
+import { ScrollView,Image, Text, View } from "react-native";
+import icon from "./assets/icon.png";
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
+
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>
+        Alam Becoming A Mobile Application Developer
+        Visit Our Company Website: www.clicksofts.com
+      </Text>
+      <Image source={{ uri: 'https://clicksofts.com/wp-content/uploads/2024/12/cropped-download-44.png' }} style={{ width: 200, height: 100 }} resizeMode="contain" />
+      <Image source={icon} style={{ width: 200, height: 200, backgroundColor: 'red' }} resizeMode="contain" />
+
     </View>
-  );
+    
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
